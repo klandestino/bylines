fe5e9f7b24115e6dc288ac82a8c0c2de55318306
Alexander Berthelsen <alexander.berthelsen@gmail.com>
2020-11-26 12:04:38 +0100

    Bump the minimum WP version to 5.3

--- bylines.php
+++ bylines.php
@@ -7,13 +7,13 @@
  * Author URI:      https://handbuilt.co
  * Text Domain:     bylines
  * Domain Path:     /languages
- * Version:         0.3.0-alpha
+ * Version:         0.3.0
  * License:         GPL v3
  *
  * @package         Bylines
  */
 
-define( 'BYLINES_VERSION', '0.3.0-alpha' );
+define( 'BYLINES_VERSION', '0.3.0' );
 
 /**
  * Bylines Plugin
@@ -37,11 +37,11 @@ define( 'BYLINES_VERSION', '0.3.0-alpha' );
  * Warn when minimum version requirements aren't met
  */
 function bylines_action_admin_notices_warn_requirements() {
-	echo '<div class="error message"><p>' . __( 'Bylines requires at least WordPress 4.4 and PHP 5.3. Please make sure you meet these minimum requirements.', 'bylines' ) . '</p></div>';
+	echo '<div class="error message"><p>' . __( 'Bylines requires at least WordPress 5.3 and PHP 5.6. Please make sure you meet these minimum requirements.', 'bylines' ) . '</p></div>';
 }
 
-if ( version_compare( $GLOBALS['wp_version'], '4.4', '<' )
-	|| version_compare( PHP_VERSION, '5.3', '<' ) ) {
+if ( version_compare( $GLOBALS['wp_version'], '5.3', '<' )
+	|| version_compare( PHP_VERSION, '5.6', '<' ) ) {
 	add_action( 'admin_notices', 'bylines_action_admin_notices_warn_requirements' );
 	return;
 }
